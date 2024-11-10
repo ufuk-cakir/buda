bde2435ce0fe3ebc0b500fc6a86ef1a3331d2d2e
Alex Dobra <alexandrundobra@gmail.com>
2024-11-10 10:40:49 +0000

    day of week like analysis

--- buda/analysis/likes.py
+++ buda/analysis/likes.py
@@ -19,18 +19,50 @@ def extract_hours(data):
     ]
     return [datetime.fromtimestamp(ts).hour for ts in timestamps]
 
-
 def count_hourly_activity(hours):
     """Count the number of likes by hour."""
     return Counter(hours)
 
+def extract_days_of_week(data):
+    """Extract days of the week from timestamps for activity analysis."""
+    timestamps = [
+        item["string_list_data"][0]["timestamp"] for item in data["likes_media_likes"]
+    ]
+    return [datetime.fromtimestamp(ts).weekday() for ts in timestamps]
 
-def display_statistics(total_likes, hourly_activity):
+def count_days_of_week_activity(days):
+    """Count the number of likes by day of the week."""
+    return Counter(days)
+
+def get_days_of_week_activity(data):
+    """Get days of the week activity data for likes."""
+    days = extract_days_of_week(data)
+    days_activity = count_days_of_week_activity(days)
+    return days_activity
+
+def display_hourly_statistics(total_likes, hourly_activity):
     """Print statistics for total likes and likes by hour."""
     print(f"Total number of likes: {total_likes}")
     print("Likes by hour of day:")
     for hour, count in sorted(hourly_activity.items()):
         print(f"{hour}:00 - {count} likes")
+
+def display_days_of_week_statistics(total_likes, days_activity):
+    """Print statistics for total likes and likes by day of the week."""
+    # create dictionary linking day of week number to day name
+    days = {
+        0: "Monday",
+        1: "Tuesday",
+        2: "Wednesday",
+        3: "Thursday",
+        4: "Friday",
+        5: "Saturday",
+        6: "Sunday",
+    }
+    print(f"Total number of likes: {total_likes}")
+    print("Likes by day of week:")
+    for day, count in sorted(days_activity.items()):
+        print(f"{days[day]}: {count} likes")
 
 
 def plot_hourly_activity(hourly_activity):
@@ -73,12 +105,17 @@ def plot_hourly_activity_circle(hourly_activity):
     plt.title("Instagram Activity by Hour")
     plt.show()
 
+def get_hourly_activity(data):
+    """Get hourly activity data for likes."""
+    hours = extract_hours(data)
+    hourly_activity = count_hourly_activity(hours)
+    return hourly_activity
 
 def analyze_likes(data):
     """Analyze likes data and display statistics and plot."""
     total_likes = calculate_total_likes(data)
     hours = extract_hours(data)
     hourly_activity = count_hourly_activity(hours)
-    display_statistics(total_likes, hourly_activity)
+    display_hourly_statistics(total_likes, hourly_activity)
     plot_hourly_activity(hourly_activity)
     plot_hourly_activity_circle(hourly_activity)
